12d04f1c08dc6819af689eff8623d175a72f3e6f
PostgreSQL <postgres@MacBook-Air.local>
2025-03-26 22:45:36 +0530

    Socket io  updated 2

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider = ({children}) => {
 
     useEffect(() => {
         if(authUser) {
-            const socket = io("https://chat-app-dep-aepc.onrender.com/landing",{
+            const socket = io("https://chat-app-dep-aepc.onrender.com",{
                 query : {
                     userId : authUser._id,
                 },
